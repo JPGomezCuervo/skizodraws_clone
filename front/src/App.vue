538bc666130ad28df7ca4c09759804ad647a0cd7
Juan Pablo <122913492+JPGomezCuervo@users.noreply.github.com>
2025-03-02 21:08:25 -0500

    add router

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,14 +1,25 @@
 <script setup>
-import NavBar from "@/components/NavBar.vue"
-import Hero from "@/components/Hero.vue"
-import Displayer from "@/components/Displayer.vue"
-</script>
+  import NavBar from "@/components/NavBar.vue";
+import { provide, ref, onMounted} from "vue";
+import { state } from "@/state.js";
 
+provide("state", state);
+
+onMounted(async() => {
+    try {
+      let res = await fetch("http://localhost:3000/collections/all");
+      state.products = await res.json();
+      res = await fetch("http://localhost:3000/collections/products/types");
+      state.product_types = await res.json();
+    } catch(error) {
+      console.error("There was an error ", error);
+    }
+});
+
+</script>
 <template>
   <NavBar/>
-  <Hero/>
-  <Displayer/>
+  <main class="bg-skizo-bg">
+    <RouterView/>
+  </main>
 </template>
-
-<style scoped>
-</style>
